22f3a22a85236477d7e6a8cab7adcf52c30a18c7
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-16 01:47:15 +0900

    feat: Iinstructor#findById 추가

--- src/instructor/domain/repository/Iinstructor.repository.ts
+++ src/instructor/domain/repository/Iinstructor.repository.ts
@@ -1,3 +1,7 @@
+import Instructor from "../instructor";
+
 export default interface IinstructorRepository {
+  findById: (id: string) => Promise<Instructor>;
+
   save: (id: string, name: string) => Promise<void>;
 }
